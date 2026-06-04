11281cd78088c641586e0f69cd7aeec097400d70
7sm2zc4rsq-tech <7sm2zc4rsq@privaterelay.appleid.com>
2026-06-04 12:34:44 +0200

    Delete public/firebase-config.example.js

--- public/firebase-config.example.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "INSERISCI_API_KEY",
-  authDomain: "INSERISCI_AUTH_DOMAIN",
-  projectId: "INSERISCI_PROJECT_ID",
-  storageBucket: "INSERISCI_STORAGE_BUCKET",
-  messagingSenderId: "INSERISCI_MESSAGING_SENDER_ID",
-  appId: "INSERISCI_APP_ID"
-};
