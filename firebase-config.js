581f0039d22bf8efd500c70b60c67cf4cac9f389
7sm2zc4rsq-tech <7sm2zc4rsq@privaterelay.appleid.com>
2026-06-04 17:58:06 +0200

    Update Firebase configuration and initialization
    Updated Firebase configuration with actual values and initialized Firebase app.

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,17 @@
-export const firebaseConfig = {
-  apiKey: "INSERISCI_API_KEY",
-  authDomain: "INSERISCI_AUTH_DOMAIN",
-  projectId: "INSERISCI_PROJECT_ID",
-  storageBucket: "INSERISCI_STORAGE_BUCKET",
-  messagingSenderId: "INSERISCI_MESSAGING_SENDER_ID",
-  appId: "INSERISCI_APP_ID"
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCMD-6NmFsIDHHIqv9loqSy_9ktOzsIDDo",
+  authDomain: "vicolo-di-mezzo2.firebaseapp.com",
+  projectId: "vicolo-di-mezzo2",
+  storageBucket: "vicolo-di-mezzo2.firebasestorage.app",
+  messagingSenderId: "419571019706",
+  appId: "1:419571019706:web:53492c557d210cc46b3783"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
