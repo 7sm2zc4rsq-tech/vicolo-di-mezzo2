3e6ce48fc64243c585306ab66cb515172c2018bd
7sm2zc4rsq-tech <7sm2zc4rsq@privaterelay.appleid.com>
2026-06-04 18:35:03 +0200

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyCMD-6NmFsIDHHIqv9loqSy_9ktOzsIDDo",
-  authDomain: "vicolo-di-mezzo2.firebaseapp.com",
-  projectId: "vicolo-di-mezzo2",
-  storageBucket: "vicolo-di-mezzo2.firebasestorage.app",
-  messagingSenderId: "419571019706",
-  appId: "1:419571019706:web:53492c557d210cc46b3783"
-};
